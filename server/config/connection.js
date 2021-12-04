1ff90c0c8b879fc76a201ae07b5bb2c10e9a79bb
Hazelton Chrome <rdupont1623@gmail.com>
2021-12-03 22:40:18 -0500

    troubleshooting database connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+"mongodb+srv://rdupont:Foot9898@cluster0.8o3ai.mongodb.net/myFirstDatabase?retryWrites=true&w=majority" || 'mongodb://localhost/googlebooks', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: false,
-});
+};
 
 module.exports = mongoose.connection;
